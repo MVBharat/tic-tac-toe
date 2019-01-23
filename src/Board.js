69de2c77489ce0de57a51123334e4920aef8ec92
MVBharat <manebharat85@gmail.com>
2019-01-23 14:02:34 +0530

    Add next player as O

--- src/Board.js
+++ src/Board.js
@@ -7,16 +7,20 @@ class Board extends Component{
 		super(props);
 		this.state = {
 			squares: Array(9).fill(null),
-			// value: null,
+			xIsNext: true
 		};
 	}
 
 	handleClick(i){
 		const squares = this.state.squares.slice();
-		squares[i] = 'X';
-		this.setState({squares: squares});
+		squares[i] = this.state.xIsNext ? 'X' : 'O'
+		this.setState({
+			squares: squares,
+			xIsNext: !this.state.xIsNext,
+		});
+
 	}
-	
+
 	renderSquare(i)	{
 	 return <Square value={this.state.squares[i]}
 		onClick={() => this.handleClick(i)}
@@ -24,7 +28,7 @@ class Board extends Component{
 	}
 
 	render(){
-		const status = "Next Player: X"
+		const status = "Next Player: " + (this.state.xIsNext ? 'X' : 'O')
 
 		return(
 			<div>
